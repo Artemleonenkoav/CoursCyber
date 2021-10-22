a7f69dfba038038ada765e9ba8c97f4f158f4df2
Artemleonenkoav <leonenkoavfin@gmail.com>
2021-10-22 21:51:18 +0300

    Variable Add

--- src/lesson2/HomeTask3.java
+++ src/lesson2/HomeTask3.java
@@ -6,6 +6,7 @@ public class HomeTask3 {
         String b = "\tмоя строка 2";
         String c = "\fмоя строка 3";
         String h = "\fмоя строка 3";
+        String s = "\fмоя строка 3";
         System.out.println(f);
         System.out.println(b);
         System.out.println(c);
